079eee97f7a3ea50bd53a66774a2d78df93f7835
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-05-31 21:31:46 +0200

    contact and protection from losses data

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,51 @@
 import React from "react";
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
 
-const ContactPage = () => {
-  return <div>Kontakt</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: "",
+    isEmpty: true,
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      value: "",
+      isEmpty: true,
+    });
+  };
+  handleChange = (e) => {
+    if (e.target.value.length > 0) {
+      this.setState({
+        value: e.target.value,
+        isEmpty: false,
+      });
+    } else {
+      this.setState({
+        value: e.target.value,
+        isEmpty: true,
+      });
+    }
+  };
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="wpisz wiadomość..."
+          ></textarea>
+          <button>Wyślij</button>
+        </form>
+        <Prompt
+          when={!this.state.isEmpty}
+          message="Masz niewypełniony formularz. Czy na pewno chcesz opuścić stronę"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
